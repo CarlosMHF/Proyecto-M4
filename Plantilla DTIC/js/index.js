039817cc3a7c8442cac64be86911be1fb8d63140
Carlos herrera <carlos.herrera@fcys.uni.edu.ni>
2024-02-02 16:31:17 -0600

    Login

--- Plantilla DTIC/js/index.js
+++ Plantilla DTIC/js/index.js
@@ -1,16 +1,29 @@
 
+
+
 function logIn(){
+    
+    let mail = document.getElementById("inputEmail").value;
 
+if (mail=="Cristopher@gmail.com") {
+ 
+    window.location.href = "../Vista Jefe/index.html"
+    alert ("Inicio exitoso")
+} else  { 
+    if (mail== "Carlos.h@gmail.com") {
+     
+        window.location.href = "../Vista Docente/index.html" 
+        alert ("Inicio exitoso")    
+    }else{
+        alert ("Credenciales incorrectas") 
+    }
+}
 
+    
+   
 
-
-
-    //window.location.href = "../Vista Jefe/index.html"
-    window.location.href = "../Vista Docente/index.html"
-
-    let email = document.getElementById("inputEmail").value;
     
 
-    alert(email)
+    
 }
 
