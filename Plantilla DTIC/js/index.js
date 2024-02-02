df1de80c5820c278a1b0e9dd49a7f39da97f79da
Carlos herrera <carlos.herrera@fcys.uni.edu.ni>
2024-02-02 15:47:01 -0600

    envio de ventana al iniciar sesion

---
+++ Plantilla DTIC/js/index.js
@@ -0,0 +1,16 @@
+
+function logIn(){
+
+
+
+
+
+    //window.location.href = "../Vista Jefe/index.html"
+    window.location.href = "../Vista Docente/index.html"
+
+    let email = document.getElementById("inputEmail").value;
+    
+
+    alert(email)
+}
+
